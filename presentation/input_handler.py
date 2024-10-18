99812ee3b666b35bdb286e5ac8e6a262f0d8b7aa
ElSan01 <141951334+ElSan01@users.noreply.github.com>
2024-10-18 15:29:22 -0300

    Update player sprite on key presses

--- presentation/input_handler.py
+++ presentation/input_handler.py
@@ -11,24 +11,43 @@ class InputHandler(IInputHandler):
 
     def __init__(self, world: GameWorld):
         self.__world = world
+        self.__direction = None
 
     def __example_method(self, keys):
+        is_moving = False
         if keys[pygame.K_w]:
-            self.__world.player.move(0,-1)
-            print("Key W")
-
+            self.__direction = "up"
+            self.__world.player.sprite.change_to_walk_up_sprite()
+            self.__world.player.move(0, -1)
+            is_moving = True
         if keys[pygame.K_s]:
-            self.__world.player.move(0,1)
-            print("Key S")
-
+            self.__direction = "down"
+            self.__world.player.sprite.change_to_walk_down_sprite()
+            self.__world.player.move(0, 1)
+            is_moving = True
         if keys[pygame.K_a]:
-            self.__world.player.move(-1,0)
-            print("Key A")
-            
+            self.__direction = "left"
+            self.__world.player.sprite.change_to_walk_left_sprite()
+            self.__world.player.move(-1, 0)
+            is_moving = True
         if keys[pygame.K_d]:
-            self.__world.player.move(1,0)
-            print("Key D")
+            self.__direction = "right"
+            self.__world.player.sprite.change_to_walk_right_sprite()
+            self.__world.player.move(1, 0)
+            is_moving = True
+
+        if is_moving == False:
+            if self.__direction == "up":
+                self.__world.player.sprite.change_to_idle_up_sprite()
+            if self.__direction == "down":
+                self.__world.player.sprite.change_to_idle_down_sprite()
+            if self.__direction == "left":
+                self.__world.player.sprite.change_to_idle_left_sprite()
+            if self.__direction == "right":
+                self.__world.player.sprite.change_to_idle_right_sprite()
+        
 
     def process_input(self):
         keys = pygame.key.get_pressed()
         self.__example_method(keys)
+
